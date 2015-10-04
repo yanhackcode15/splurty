e65010d38e23271e4208e1bb7cc4e8ef940fcbeb
Yan Hu <iamyanhu@gmail.com>
2015-10-04 19:24:33 +0000

    Added input validation for quote and author

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maxinum => 140, :minimum => 3 }
+	validates :author, :presence => true, :length => { :maxinum => 50, :minimum => 3 }
+
 end
