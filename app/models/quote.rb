7e60c9e7800d5cd2ad0b7d96027c98915e15507b
Yan Hu <iamyanhu@gmail.com>
2015-11-12 06:37:15 +0000

    added the unique_tag method in the model using TDD

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,13 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maxinum => 140, :minimum => 3 }
 	validates :author, :presence => true, :length => { :maxinum => 50, :minimum => 3 }
-
+	
+	def unique_tag
+		tag = ""
+		self.author.split(" ").each do |name|
+			tag+=name[0]
+		end
+		tag+="#"
+		tag+="#{self.id.to_s}"
+	end 
 end
