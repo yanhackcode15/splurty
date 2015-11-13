4525bbbafcad6a28de2c9cc65e151b36718a3e08
Yan Hu <iamyanhu@gmail.com>
2015-11-13 09:14:49 +0000

    Added controller tests for the quote show page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,12 +12,23 @@ class QuotesController < ApplicationController
 		redirect_to root_path
 	end
 
+	def about
+	end
+	
+	def show
+		@quote = Quote.where(:id=> params[:id]).first
+		if @quote.blank?
+			render :text => "Not Found", :status => :not_found
+		end
+	end
+
 	private
 
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
 	end
 
-	def about
-	end
+
+
+
 end
