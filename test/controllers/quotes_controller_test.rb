f4e2bd5228931fd886e6e6de4f42b8bef1471f92
Yan Hu <iamyanhu@gmail.com>
2015-11-14 23:09:24 +0000

    Added FactoryGirl gem and started using it

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
   test "quote show page" do
-    quote = Quote.create(:author=> 'Yan Hu', :saying => 'You only yolo once.')
+    quote = FactoryGirl.create(:quote)
   	get :show, :id => quote.id
   	assert_response :success
   end
